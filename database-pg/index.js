6e603dc65c96c4d7f4a36ebcdc3a45e2250f4765
ihebmoujahed <ihebmoujahed@gmail.com>
2024-11-04 12:31:02 +0100

    add db

--- database-pg/index.js
+++ database-pg/index.js
@@ -1,13 +1,18 @@
-const { Client } = require('pg')
+const { Client } = require('pg');
+
+const connectionString = 'postgresql://erp_olfa_user:QBDKLOSgofnexVl14WLmaZJFriJiLqTw@dpg-cskasn8gph6c73a8elqg-a.oregon-postgres.render.com/erp_olfa';
 
 const client = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  port: '5432',
-  password: 'root',
-  database: 'school',
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Adjust based on your security requirements
+  },
 });
 
+// Connect to the database
 client.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
 
+// Export the client
 module.exports = client;
