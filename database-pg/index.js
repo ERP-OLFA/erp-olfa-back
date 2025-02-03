94ff89a4868ae6f7463a375f33ef6b05192660f9
ihebmoujahed <ihebmoujahed@gmail.com>
2025-02-03 09:17:53 +0100

    new db

--- database-pg/index.js
+++ database-pg/index.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 // New connection string with updated credentials
-const connectionString = 'postgresql://erp_olfa_databsv1_2_user:8hz75BufABPbPdfLjlWt5GG8LsqOfDdy@dpg-cts07mjtq21c73946p50-a.oregon-postgres.render.com/erp_olfa_databsv1_2';
+const connectionString = 'postgresql://erp_olfa_databsv1_3_user:LUPtVuYwcbszqPmmhV1Lx1ma54WiNlvv@dpg-cug7hr5ds78s738d33lg-a.oregon-postgres.render.com/erp_olfa_databsv1_3';
 
 const client = new Client({
   connectionString: connectionString,
@@ -12,7 +12,7 @@ const client = new Client({
 
 // Connect to the database
 client.connect()
-  .then(() => console.log('Connected to the database'))
+  .then(() => console.log('Connected to the new database'))
   .catch(err => console.error('Connection error', err.stack));
 
 // Export the client
